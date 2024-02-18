5fa4b9c6a7702fec9e57efa10cecf110f6ca867e
Gustavo Duarte <gustavo64duarte@gmail.com>
2024-02-17 21:57:40 -0300

    Create AudioManager.cs

---
+++ FPS MULTIPLAYER/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source =  gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+   public void Play (string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s == null)
+        {
+            Debug.LogWarning("Sound : " + name + "not found! ");
+            return;
+        }
+        s.source.Play();
+    }
+
+    public void PlayOneShot (string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s == null)
+        {
+            Debug.LogWarning("Sound : " + name + "not found! ");
+            return;
+        }
+
+        s.source.PlayOneShot(s.source.clip);
+    }
+}
